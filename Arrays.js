ebe83ca29bcee93589fbce3d2021e94cb6487683
Gayatri <gayatribhangle30@gmail.com>
2025-03-22 00:15:50 +0530

    Array operations

--- Arrays.js
+++ Arrays.js
@@ -16,11 +16,13 @@ for(let i=0; i<fruits.length; i++){
 
 
 // Removing elements
+console.log("Pop() and Shift()");
 fruits.pop();       // Removes last element (end)
 fruits.shift();     // Removes firsts element (beginning)
 console.log(fruits);
 
-// Iterating arrat using for each loop
+// Iterating array using for each loop
+console.log("Iterating array using foreach loop");
 fruits.forEach((fruit, index) => 
     console.log(fruit)
 )
@@ -29,14 +31,18 @@ fruits.forEach((fruit, index) =>
 let numbers = [1, 2, 3, 4, 5];
 
 // Map -> Transforms each element
+
+console.log("Map() function: ");
 let squared = numbers.map(num => num ** 2);
 console.log(squared);
 
 // Filter -> Selects elements based on condition
+console.log("Filter() function: ");
 let even = numbers.filter(num => num % 2 === 0);
 console.log(even);
 
 // Reduce -> Aggregate to a single vale
+console.log("Reduce() function: ");
 let sum = numbers.reduce((acc, num) => acc + num, 0);
 console.log(sum);
 
