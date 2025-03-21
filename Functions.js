2648b859da4ee9813d515fed9eea6e747fc6993e
Gayatri <gayatribhangle30@gmail.com>
2025-03-22 01:26:51 +0530

    Callback function

--- Functions.js
+++ Functions.js
@@ -1,1 +1,29 @@
 // Regular function
+function greet() {
+    console.log("Hello world")
+}
+
+// function with parameters and return statement
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+// Arrow function
+const greetArrow = (name) => {
+    console.log("Hello", name);
+
+}
+
+// function call
+greet();
+console.log("Sum is: ", sum(2, 5));
+greetArrow("Alice");
+
+// function as a parameter
+function process(func, value) {
+    func(value);
+}
+
+console.log("Function as a parameter");
+
+process(greet, "Alice");
