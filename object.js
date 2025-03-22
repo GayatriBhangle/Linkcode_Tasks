8e4ea9b4a407a976423436cb019df26f1cb32843
Gayatri <gayatribhangle30@gmail.com>
2025-03-22 10:43:11 +0530

    object.js file created

---
+++ object.js
@@ -0,0 +1,21 @@
+let student = {
+    name: "Gayatri",
+    age: 20,
+    Address: {
+        city: "Pune",
+        pinCode: 411015
+    },
+    College: "PICT",
+    hobbies: ["Dancing", "Coding", "Reading"]
+}
+
+// Accessing object properties
+console.log(student.name);      // Gayatri
+console.log(student.Address);      // city: 'Pune' 
+console.log(student.Address.city);  // Pune
+console.log(student.hobbies[1]);
+
+let car = {
+    brand: "Toyota",
+    model: 
+}
