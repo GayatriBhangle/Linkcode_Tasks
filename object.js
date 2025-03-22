715cbd43639a72f7c4bf28bebe5fa0e870b4be54
Gayatri <gayatribhangle30@gmail.com>
2025-03-22 10:51:20 +0530

    Object oeprations performed

--- object.js
+++ object.js
@@ -13,9 +13,23 @@ let student = {
 console.log(student.name);      // Gayatri
 console.log(student.Address);      // city: 'Pune' 
 console.log(student.Address.city);  // Pune
-console.log(student.hobbies[1]);
+console.log(student.hobbies[1]);    // Coding
 
-let car = {
-    brand: "Toyota",
-    model: 
-}
+// Modifying object properties
+student.hobbies[2] = "Trecking";
+console.log(student.hobbies);      // [ 'Dancing', 'Coding', 'Trecking' ]
+
+// Adding new property
+student.isRankHolder = true;
+console.log(student);
+
+// output
+
+// {
+//     name: 'Gayatri',
+//     age: 20,
+//     Address: { city: 'Pune', pinCode: 411015 },
+//     College: 'PICT',
+//     hobbies: [ 'Dancing', 'Coding', 'Trecking' ],
+//     isRankHolder: true
+//   }
